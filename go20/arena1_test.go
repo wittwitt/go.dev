3a397edb7e0f76f1a678a9988fccc312cf9b65a3
Witt <i5d@live.com>
2023-04-29 11:01:51 +0800

    log

---
+++ go20/arena1_test.go
@@ -0,0 +1,11 @@
+package go20
+
+import "testing"
+
+//  export GOEXPERIMENT=arenas
+// GOEXPERIMENT=arenas go run main.go
+// go run main.go -tags goexperiment.arenas
+
+func Test_arena1(t *testing.T) {
+
+}
